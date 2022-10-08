67d6ee6cf8f7bf88bf32477a8c67078f92f1af7d
jemila ibrahim <jemiladotto@gmail.com>
2022-10-08 15:05:10 -0700

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-
 /**
  * swap_ints - Swap two integers in an array.
  * @a: The first integer to swap.
@@ -13,13 +12,11 @@ void swap_ints(int *a, int *b)
 	*a = *b;
 	*b = tmp;
 }
-
 /**
  * selection_sort - Sort an array of integers in ascending order
- *                  using the selection sort algorithm.
+ * using the selection sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
- *
  * Description: Prints the array after each swap.
  */
 void selection_sort(int *array, size_t size)
